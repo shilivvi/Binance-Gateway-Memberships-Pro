e85de58328f9a301fdadbf9f3a32124e0c76cf94
shilivvi <shilivvi98@gmail.com>
2022-12-15 17:13:51 +0300

    Init

---
+++ pmpro-binance-gateway.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: 		Binance Gateway Addon for Memberships Pro
+Plugin URI: 		https://test.com
+Description: 		Description
+Version: 			1.1.8
+Author: 			Author
+License: 			GPLv2
+License URI: 		http://www.gnu.org/licenses/gpl-2.0.html
+*/
