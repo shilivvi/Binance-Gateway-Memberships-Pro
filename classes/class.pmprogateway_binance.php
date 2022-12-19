f5edf8cc71b298c7fcbef81b0066ef4970c25b9c
shilivvi <shilivvi98@gmail.com>
2022-12-19 13:46:01 +0300

    Changed checkout page and checkout rules

--- classes/class.pmprogateway_binance.php
+++ classes/class.pmprogateway_binance.php
@@ -19,6 +19,17 @@ class PMProGateway_binance extends PMProGateway
         //add fields to payment settings
         add_filter('pmpro_payment_options', array('PMProGateway_binance', 'pmpro_payment_options'));
         add_filter('pmpro_payment_option_fields', array('PMProGateway_binance', 'pmpro_payment_option_fields'), 10, 2);
+
+        $gateway = pmpro_getOption('gateway');
+        if($gateway == 'binance')
+        {
+            add_filter('pmpro_include_billing_address_fields', '__return_false');
+            add_filter('pmpro_include_payment_information_fields', '__return_false');
+            add_filter('pmpro_required_billing_fields', array('PMProGateway_binance', 'pmpro_required_billing_fields'));
+            add_filter('pmpro_checkout_default_submit_button', array('PMProGateway_binance', 'pmpro_checkout_default_submit_button'));
+            //add_filter('pmpro_checkout_order', array('PMProGateway_example', 'pmpro_checkout_order'));
+            //add_filter('pmpro_include_cardtype_field', array('PMProGateway_example', 'pmpro_include_billing_address_fields'));
+        }
     }
 
     /**
@@ -96,5 +107,65 @@ class PMProGateway_binance extends PMProGateway
             </td>
         </tr>
         <?php
+    }
+
+    /**
+     * Remove required billing fields
+     */
+    public static function pmpro_required_billing_fields($fields)
+    {
+        unset($fields['bfirstname']);
+        unset($fields['blastname']);
+        unset($fields['baddress1']);
+        unset($fields['bcity']);
+        unset($fields['bstate']);
+        unset($fields['bzipcode']);
+        unset($fields['bphone']);
+        unset($fields['bemail']);
+        unset($fields['bcountry']);
+        unset($fields['CardType']);
+        unset($fields['AccountNumber']);
+        unset($fields['ExpirationMonth']);
+        unset($fields['ExpirationYear']);
+        unset($fields['CVV']);
+
+        return $fields;
+    }
+
+    /**
+     * Swap in our submit buttons.
+     */
+    static function pmpro_checkout_default_submit_button($show)
+    {
+        global $gateway, $pmpro_requirebilling;
+
+        error_log(print_r(pmpro_getOption('binance_api_key'), 1));
+
+        //show our submit buttons
+        ?>
+        <span id="pmpro_submit_span">
+				<input type="hidden" name="submit-checkout" value="1" />
+				<input type="submit" class="<?php echo pmpro_get_element_class( 'pmpro_btn pmpro_btn-submit-checkout', 'pmpro_btn-submit-checkout' ); ?>" value="<?php if($pmpro_requirebilling) { _e('Check Out with Binance Pay', BINANCEPMP ); } else { _e('Submit and Confirm', BINANCEPMP );}?> &raquo;" />
+			</span>
+        <?php
+
+        //don't show the default
+        return false;
+    }
+
+    /**
+     * Process checkout.
+     */
+    function process(&$order)
+    {
+        if(empty($order->code)){
+            $order->code = $order->getRandomCode();
+        }
+
+        //just save, the user will go to binance to pay
+        $order->status = "pending";
+        $order->saveOrder();
+
+        return true;
     }
 }
