319049bf3eeec0010c5d1ab994d24cacbc83fb01
shilivvi <shilivvi98@gmail.com>
2022-12-20 20:54:57 +0300

    Added functionality for webhook

--- services/binancepay-webhook.php
+++ services/binancepay-webhook.php
@@ -14,9 +14,69 @@ global $wpdb, $gateway_environment, $logstr;
 
 pmpro_doing_webhook('binance', true);
 
-error_log(print_r($_REQUEST, 1));
+//error_log(print_r($_REQUEST, 1));
 
+$bizType = pmpro_getParam('bizType', 'REQUEST');
+$bizStatus = pmpro_getParam('bizStatus', 'REQUEST');
+$data = pmpro_getParam('data', 'REQUEST');
 
+if(isset($bizType) && $bizType == 'PAY'){
+    if(isset($bizStatus) && $bizStatus == 'PAY_SUCCESS'){
+        if(isset($data['merchantTradeNo'])){
+            $merchantTradeNo = $data['merchantTradeNo'];
+            $morder = new MemberOrder($merchantTradeNo);
+            $morder->getUser();
+            $morder->getMembershipLevel();
+
+            if (!empty ($morder) && !empty($morder->status) && $morder->status === 'success') {
+                // Checkout was already processed
+            } else {
+                // Extend membership if renewal.
+                // Added manually because pmpro_checkout_level filter is not run.
+                $morder->membership_level = pmpro_checkout_level_extend_memberships($morder->membership_level);
+
+                // Set the start date to current_time('mysql') but allow filters (documented in preheaders/checkout.php)
+                $startdate = apply_filters("pmpro_checkout_start_date", "'" . current_time('mysql') . "'", $morder->user_id, $morder->membership_level);
+
+                // Fix expiration date
+                if (!empty($morder->membership_level->expiration_number)) {
+                    $enddate = "'" . date_i18n("Y-m-d", strtotime("+ " . $morder->membership_level->expiration_number . " " . $morder->membership_level->expiration_period, current_time("timestamp"))) . "'";
+                } else {
+                    $enddate = "NULL";
+                }
+
+                $custom_level = array(
+                    'user_id' => $morder->user_id,
+                    'membership_id' => $morder->membership_level->id,
+                    'code_id' => '',
+                    'initial_payment' => $morder->membership_level->initial_payment,
+                    'billing_amount' => $morder->membership_level->billing_amount,
+                    'cycle_number' => $morder->membership_level->cycle_number,
+                    'cycle_period' => $morder->membership_level->cycle_period,
+                    'billing_limit' => $morder->membership_level->billing_limit,
+                    'trial_amount' => $morder->membership_level->trial_amount,
+                    'trial_limit' => $morder->membership_level->trial_limit,
+                    'startdate' => $startdate,
+                    'enddate' => $enddate
+                );
+
+                if (pmpro_changeMembershipLevel($custom_level, $morder->user_id) !== false) {
+                    $morder->status = "success";
+                    $morder->saveOrder();
+                    do_action("pmpro_after_checkout", $morder->user_id, $morder);
+                }
+            }
+        }
+    }elseif (isset($bizStatus) && $bizStatus == 'PAY_CLOSED'){
+        if(isset($data['merchantTradeNo'])){
+            $merchantTradeNo = $data['merchantTradeNo'];
+            $morder = new MemberOrder($merchantTradeNo);
+            $morder->updateStatus('failed');
+        }
+    }
+}
+
+pmpro_unhandled_webhook();
 
 // return 200
 $returnData = array(
@@ -26,3 +86,4 @@ $returnData = array(
 http_response_code(200);
 header("Content-type: application/json; charset=utf-8");
 echo json_encode($returnData);
+exit;
