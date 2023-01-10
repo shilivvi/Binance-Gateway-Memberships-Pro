ba4a216c6d412334f082c788f2a484682998e5a3
shilivvi <shilivvi98@gmail.com>
2023-01-10 13:13:15 +0300

    Added function to send requests to BinancePay API

--- services/binancepay-client.php
+++ services/binancepay-client.php
@@ -60,4 +60,54 @@ class BinancePayClient
 
         return $nonce;
     }
+
+    /**
+     * Do request to binance pay API
+     *
+     * @param string $endPoint
+     * @return false|object
+     */
+    private function doRequest(string $endPoint)
+    {
+        // Create url for request
+        $urlRequest = $this->apiUrl . $endPoint;
+
+        // Get nonce
+        $nonce = $this->getNonce();
+
+        // Get timestamp
+        $timestamp = round(microtime(true) * 1000);
+
+        // Convert request data to jsom format
+        $requestData = json_encode($this->clintPDO);
+
+        // Create payload
+        $payload = $timestamp . "\n" . $nonce . "\n" . $requestData . "\n";
+
+        // Generate signature
+        $signature = strtoupper(hash_hmac('SHA512', $payload, $this->secretKey));
+
+        //curl
+        $ch = curl_init();
+        $headers = array();
+        $headers[] = "Content-Type: application/json";
+        $headers[] = "BinancePay-Timestamp: $timestamp";
+        $headers[] = "BinancePay-Nonce: $nonce";
+        $headers[] = "BinancePay-Certificate-SN: $this->apiKey";
+        $headers[] = "BinancePay-Signature: $signature";
+
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($ch, CURLOPT_URL, $urlRequest);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $requestData);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        $result = json_decode(curl_exec($ch));
+        curl_close($ch);
+
+        if ($result->status == 'SUCCESS') {
+            return $result;
+        } else {
+            return false;
+        }
+    }
 }
