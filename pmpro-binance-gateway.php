98c6a313cc8f955e0c57ff410ea3213ce586013f
shilivvi <shilivvi98@gmail.com>
2023-01-13 16:38:31 +0300

    Changed plugin version

--- pmpro-binance-gateway.php
+++ pmpro-binance-gateway.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: 		Binance Gateway Addon for Memberships Pro
 Description: 		Binance Gateway Addon for Memberships Pro
-Version: 			0.4
+Version: 			0.4.2
 Author: 			PROYKEY
 License: 			GPLv2
 License URI: 		https://proykey.by
